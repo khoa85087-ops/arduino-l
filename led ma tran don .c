85013c50e4d036147e7680ffd062a691fd250092
Nguyễn Anh Khoa <khoa85087@gmail.com>
2026-04-07 12:15:52 +0700

    Create led ma tran don .c

---
+++ led ma tran don .c
@@ -0,0 +1,37 @@
+#include <bai_2.h>
+
+#include <def_877a.h>
+#use delay(clock=4000000)
+#FUSES NOWDT,HS,NOPUT,NOPROTECT,NODEBUG,NOBROWNOUT,NOLVP,
+signed int i,j;
+int kq,xoay;
+void main()
+{
+trisa=0x00;
+trisd=0x00;
+trise=0x00;
+trisc=0x00;
+//set RA1 (cot 1)
+porta=0b00000010;
+porte=0b00000000;
+portc=0b00000000;
+   while(TRUE)
+   {
+   kq=0x00;
+     
+      for (i=8;i>=1;i--){
+      xoay=0x01;
+      Portd=xoay|kq;
+      delay_ms(200);
+      for (j=1;j<i;j++){
+      xoay=xoay<<1;
+      Portd=kq|xoay;
+      delay_ms(200);
+      }
+      kq=portD;
+      }
+
+
+   }
+   }
+
