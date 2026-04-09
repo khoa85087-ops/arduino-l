84dde353c544ce8d6c26c1abba5fe30768f930aa
Nguyễn Anh Khoa <khoa85087@gmail.com>
2026-04-09 10:49:07 +0700

    Update led 9999.c

--- led 9999.c
+++ led 9999.c
@@ -1,3 +1,4 @@
+
 #include <bt_mau.h>
 #include <def_877a.h>
 #use delay(clock=4000000)
@@ -6,7 +7,7 @@ const unsigned int LED[10]= {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
 void chan_1();
 void chan_2();
 void chan_3();
-void don_vi(int l,int m,int h );
+void hien_thi(int i,int l,int m,int h );
 void main()
 {
  
@@ -22,15 +23,17 @@ void main()
  for (int h=0;h<=9;h++){
  for (int m=0;m<=9;m++){
     for (int l=0;l<=9;l++){
-          don_vi(l,m,h);
-     }
+      for (int i=0;i<=9;i++){
+          hien_thi(i,l,m,h);
+            }
+         }
 
   
        }
       }
-      }
+   }
        
-  }
+ }
 
 void chan_1(){
 
@@ -56,28 +59,27 @@ void chan_4(){
                       portC=  0x00;
                    portE=  0x00;
 }
-void don_vi(int l,int m,int h){
+void hien_thi(int i,int l,int m,int h){
 
-            for(int i=0;i<=9;i++)
-                {
+            
                 
                      chan_1();
-                    Portd= LED[i];
-                     delay_ms(10);
+                     Portd= LED[i];
+                     delay_ms(1);
+                     
                      chan_2();
                      portd=LED[l];
+                     delay_ms(1);
                      
-                     delay_ms(10);
                      chan_3();
-                     portd=LED[m];
+                     portd=LED[m];               
+                     delay_ms(1);
                      
-                     delay_ms(10);
                      chan_4();
                      portd=LED[h];
-                     
-                     delay_ms(10);
+                     delay_ms(1);
 
                    
                
-             }
+          
 }
