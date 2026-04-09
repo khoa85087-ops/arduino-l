6058b99fdb1addba035346c3d72e707d3004945d
Nguyễn Anh Khoa <khoa85087@gmail.com>
2026-04-09 20:23:40 +0700

    Update led 9999.c

--- led 9999.c
+++ led 9999.c
@@ -7,6 +7,7 @@ const unsigned int LED[10]= {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
 void chan_1();
 void chan_2();
 void chan_3();
+void chan_4();
 void hien_thi(int i,int l,int m,int h );
 void main()
 {
